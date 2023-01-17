ce90c75263defbfbdd5ec7bb1cac30ea1ee5e398
함초롬 <chorom.m.ham@gmail.com>
2023-01-18 06:48:34 +0900

    💄 style: Change background color

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "gray.100",
+      },
+    },
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
