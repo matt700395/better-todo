63374b7dafed0b69e5df6792c405168134fc4725
함초롬 <chorom.m.ham@gmail.com>
2023-01-19 16:26:58 +0900

    💄 style: Change card style

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -4,7 +4,7 @@ import { Flex, Card } from "@chakra-ui/react";
 function TodoTemplate({ children }) {
   return (
     <Flex w="full" h="100vh" justifyContent="center" alignItems="center">
-      <Card w="md" h="xl" borderRadius="xl" pos="relative">
+      <Card w="md" h="xl" borderRadius="xl" pos="relative" overflow="hidden">
         {children}
       </Card>
     </Flex>
