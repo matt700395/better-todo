60d210fe0d0c9f40102cd471cef6e85dbfa7342f
matt700395 <matt7003@korea.ac.kr>
2023-01-30 20:41:05 +0900

    useTodo edit

--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -1,10 +1,14 @@
+import { useToast } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import { base } from './api';
 
 export default function useTodo() {
+    const toast = useToast()
+
     const [ isLoading, setIsLoading ] = useState(false);
     const [isError, setIsError] = useState(false);
     const[todoList, setTodoList] = useState(null);
+
 
     const getTodoList = async () => {
         setIsError(false);
@@ -20,6 +24,33 @@ export default function useTodo() {
         setIsLoading(false);
     };
 
+    const createTodo = async (text) => {
+        try{
+            const response = await base().post("/Todo", {
+                fields: {
+                    text: text,
+                    isDone: false,
+                },
+            });
+            setTodoList([...todoList, response.data]);
+            toast({
+                title: "할 일이 추가되었습니다.",
+                status: "success",
+                duration: 2000,
+                position: "bottom",
+            });
+
+        } catch (error) {
+            toast({
+                title: "에러발생했습니다.",
+                status:'error',
+                duration:2000,
+                position: 'bottom',
+            })
+ 
+        }
+    };
+    
     useEffect(()=>{
         getTodoList();
     },[]);
